3804324248496764f0cab08b75c75b737526d8a5
Dibola880 <bonganembele01@gmail.com>
2026-09-22 17:03:18 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 import os
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app():
@@ -34,6 +37,7 @@ def create_app():
     app.config["SESSION_COOKIE_SAMESITE"] = "Lax"
 
     db.init_app(app)
+    migrate.init_app(app, db)
 
     from .routes import bp
     app.register_blueprint(bp)
